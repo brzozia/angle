8046583925d6c76546041a4355ecc8a0555b022b
brzozows <nbrzoza@student.agh.edu.pl>
2025-04-29 19:36:07 +0200

    works

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <RH_ASK.h>
 #include <SPI.h>
+#include <ESP8266WiFi.h>
 
 // put function declarations here:
 RH_ASK driver(2000, D7, D6, D5);
@@ -8,31 +9,36 @@ const uint8_t PROX_SENSOR_PIN = 16; // GPIO16
 
 void setup()
 {
-  // put your setup code here, to run once:
+  WiFi.mode(WIFI_OFF);
+  if (WiFi.getMode() == WIFI_OFF)
+  {
+    Serial.println(F("\nWifi mode is WIFI_OFF, until it is explicitly changed"));
+  }
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(PROX_SENSOR_PIN, INPUT);
   Serial.begin(9600);
-  
+
   if (!driver.init())
     Serial.println("initialised");
 }
 
 void loop()
 {
-  while (1){
-    delay(5);
-    if (digitalRead(PROX_SENSOR_PIN) == LOW){  
-      Serial.println("dupa");    
-      Serial.println(digitalRead(PROX_SENSOR_PIN));                   // Check the sensor output{
+  while (1)
+  {
+    if (digitalRead(PROX_SENSOR_PIN) == LOW)
+    {
+      Serial.println("wlelcial-pieniazek"); // Check the sensor output{
       digitalWrite(LED_BUILTIN, HIGH);
-      const char *msg = "ala";
-      boolean send_info = driver.send((uint8_t *)msg, strlen(msg));
-      if (send_info)
+
+      const uint8_t msg = 1;
+      if (driver.send(&msg, 8))
       {
-        Serial.print(8);
+        Serial.print("sent!");
       }
       driver.waitPacketSent();
       digitalWrite(LED_BUILTIN, LOW);
     }
+    delay(5);
   }
 }
